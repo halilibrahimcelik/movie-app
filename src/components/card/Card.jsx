3220788f1dedff723737abdc4ec08e328fe9ecf4
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-17 19:01:44 +0300

    Finisihing styling of the overview element

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,16 +1,20 @@
 import React, { Fragment } from 'react'
 import styles from "./Card.module.scss";
-import {FaImdb} from "react-icons/fa"
+import {FaImdb} from "react-icons/fa";
+import {GiClick} from "react-icons/gi"
+
 const Card = ({title,img,overview,rate}) => {
   return (
     <Fragment>
     <li className={styles.card} >
         <img src={`https://image.tmdb.org/t/p/w300${img}`} alt="" />
         <div>
-            <p>{title} </p>
+            <p  className={styles.title}>{title} </p>
            
             <span>  <FaImdb className={styles.icon} /> {rate} </span>
         </div>
+        <p className={styles.overview} > {overview} <span> for more details.. </span>   <GiClick className={styles.icon}/> </p>
+       
     </li>
 
     </Fragment>
