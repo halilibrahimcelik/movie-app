917c35a9425fbfd413c502067014efc5426d6c1b
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-17 17:24:09 +0300

    structuring custom hook

---
+++ src/context/authContext.jsx
@@ -0,0 +1,47 @@
+
+import {createContext,useContext, useEffect, useState} from "react";
+
+export const  AuthContext=createContext({
+  movieData:"" 
+});
+
+
+//?Defining our custom Hook
+export  const  useAuthContext=()=>{
+    return useContext(AuthContext)
+}
+
+
+const API_KEY=process.env.REACT_APP_APP_KEY;
+const url=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
+
+const AuthContextProvider=(props)=>{
+const [movieData,setMovieData]=useState("");
+
+const fetchData=async()=>{
+ try {
+  
+    const response= await fetch(url)
+    if(!response.ok){
+        throw new Error("Something went wrong")
+    }else{
+        const data= await response.json();
+        setMovieData(data)
+    }
+
+ } catch (error) {
+    console.log(error)
+ }
+}
+
+useEffect(()=>{fetchData()},[])
+
+const contextValue={
+movieData:movieData
+}
+    return (<AuthContext.Provider value={contextValue}>
+        {props.children}
+    </AuthContext.Provider>)
+};
+
+export default AuthContextProvider;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import MovieCard from '../../components/movieCard/MovieCard';
 import SearchForm from '../../components/searchForm/SearchForm';
+import { useAuthContext } from '../../context/authContext';
 import Container from '../../UI/Container';
 import styles from "./Home.module.scss"
 
 const Home = () => {
+  const {movieData}=useAuthContext();
+console.log(movieData)
   return (
     <Fragment>
       <Container>
