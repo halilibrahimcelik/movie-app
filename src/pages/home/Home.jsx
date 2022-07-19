5929d31b0f6246fb7c7ae242205adc6c5f8e21d3
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-19 09:24:10 +0300

    Adding visual for no data fetching.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import Container from '../../UI/Container';
 import styles from "./Home.module.scss";
 import { ToastContainer } from 'react-toastify';
 import spinner from "../../assests/gif hour glass.gif";
+import image from "../../assests/lost.gif"
 
 const Home = () => {
   const {movieData,fetchData}=useAuthContext();
@@ -17,6 +18,7 @@ if(results?.length===0 ){
   return<Fragment>
     <section className={styles.wrapper} >
     <h4 style={{color:"white", textAlign:"center"}} > Unfortunetly no relatable movie list has been found based on the query  </h4>
+     <div className={styles.imageDiv} ><img src={image} alt="lost-gif"  /></div>
       <button className={styles.button} onClick={()=> fetchData()}>Go Back</button>
     </section>
       
