ff7c626210b342e0bb38ac4e6579329e031e9a54
halilibrahimcelik <halil.celik139@gmail.com>
2022-08-11 19:39:40 +0300

    fixed a bug

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,18 +1,17 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react'
-import { useAuthContext } from '../../context/authContext';
-import PasswordModule from '../passwordReset/PasswordModule';
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { useAuthContext } from "../../context/authContext";
+import PasswordModule from "../passwordReset/PasswordModule";
 import styles from "./LoginForm.module.scss";
-import { ToastContainer, toast } from 'react-toastify';
-import{useNavigate,Navigate} from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate, Navigate } from "react-router-dom";
 const LoginForm = () => {
-  const { toggleSign, setUserName, login ,googleSignIn,isLoggedIn,user} = useAuthContext();
+  const { toggleSign, setUserName, login, googleSignIn, isLoggedIn, user } =
+    useAuthContext();
 
   const [initialName, setName] = useState("");
   const emailInput = useRef("");
   const passwordInput = useRef("");
   const FIREBASE_WEB_KEY = process.env.REACT_APP_FIREBASE_WEB_KEY;
-
-
 
   const handleSubmit = (e) => {
     const enteredName = toggleSign ? initialName : "";
@@ -24,10 +23,10 @@ const LoginForm = () => {
     let url;
     if (toggleSign) {
       //?REGISTRATION
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_WEB_KEY}`
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_WEB_KEY}`;
     } else {
       //?LOGIN
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_WEB_KEY}`
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_WEB_KEY}`;
     }
 
     fetch(url, {
@@ -39,92 +38,94 @@ const LoginForm = () => {
         returnSecureToken: true,
       }),
       headers: {
-        "Content-Type": "application/json"
-      }
-    }).then(response => {
-
-      if (!response.ok) {
-        //?we got and eror
-        return response.json().then(data => {
-          //?show your error modal
-     
-          let errorMessage = "Authentication Failed";
-          if (data && data.error && data.error.message) {
-            errorMessage = data.error.message;
-          }
-          toast.error(`${errorMessage}`, {
-            position: "top-left",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            });
-          throw new Error(errorMessage)
-        })
-      } else {
-        return response.json()
-      }
-    }).then(data => {
-      //!we get successfully our data;
-   
-      toast.success("You have succesfully logged in!", {
-        position: "top-left",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        });
- 
-      setUserName(data.displayName);
-      login(data.idToken)
-
-
+        "Content-Type": "application/json",
+      },
     })
+      .then((response) => {
+        if (!response.ok) {
+          //?we got and eror
+          return response.json().then((data) => {
+            //?show your error modal
+
+            let errorMessage = "Authentication Failed";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            toast.error(`${errorMessage}`, {
+              position: "top-left",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+            });
+            throw new Error(errorMessage);
+          });
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        //!we get successfully our data;
+
+        toast.success("You have succesfully logged in!", {
+          position: "top-left",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+
+        setUserName(data.displayName);
+        login(data.idToken);
+      });
+  };
+
+  if (user?.displayName || isLoggedIn) {
+    return <Navigate to="/" />;
   }
-
-if(user?.displayName ||isLoggedIn){
-
-  return <Navigate to="/" />
-
-   
-}
   return (
     <Fragment>
-      <section className={styles.wrapper} >
-        <form className={styles.form} onSubmit={handleSubmit} >
-          {toggleSign && <>
-            <label htmlFor="nameInput">Name
-            </label>
-            <input type="text" id="nameInput" onChange={e => setName(e.target.value)} />
-          </>}
-          <label htmlFor="emailInput">Email
-          </label>
+      <section className={styles.wrapper}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          {toggleSign && (
+            <>
+              <label htmlFor="nameInput">Name</label>
+              <input
+                type="text"
+                id="nameInput"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </>
+          )}
+          <label htmlFor="emailInput">Email</label>
           <input type="email" id="emailInput" ref={emailInput} />
           <label htmlFor="passwordInput">Password</label>
-          <input type="password" id='passwordInput' ref={passwordInput} />
-          <button   > {toggleSign ? "REGISTER" : "LOGIN"} </button>
+          <input type="password" id="passwordInput" ref={passwordInput} />
+          <button> {toggleSign ? "REGISTER" : "LOGIN"} </button>
         </form>
-       {!toggleSign && <button 
-       className={styles["password-reset-btn"]} 
-       data-bs-toggle="modal"
-        data-bs-target="#edit-modal"
-       >Forgot your password</button>}
+        {!toggleSign && (
+          <button
+            className={styles["password-reset-btn"]}
+            data-bs-toggle="modal"
+            data-bs-target="#edit-modal"
+          >
+            Forgot your password
+          </button>
+        )}
         <button
           className={styles["login-with-google-btn"]}
           onClick={googleSignIn}
         >
           {toggleSign ? "Register with Google" : "Sign with Google"}
-
         </button>
       </section>
-<PasswordModule/>
-
+      <PasswordModule />
     </Fragment>
-  )
-}
+  );
+};
 
 export default LoginForm;
--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,133 +1,138 @@
-
-import {createContext,useContext, useEffect, useState,useCallback} from "react";
-import { GoogleAuthProvider,signInWithPopup,signOut,onAuthStateChanged  } from "firebase/auth";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+} from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { auth } from "../Firebase";
-export const  AuthContext=createContext({
-  movieData:"",
-  setQuery:()=>{},
-  setToggleSign:()=>{},
-  toggleSign:null,
-  userName:"",
-  setUserName:()=>{},
-  token:"",
-  login:(token)=>{},
-  logout:()=>{},
-  isLoggedIn:false,
-  googleSignIn:()=>{},
-  googleSingOut:()=>{},
-  user:""
-
+export const AuthContext = createContext({
+  movieData: "",
+  setQuery: () => {},
+  setToggleSign: () => {},
+  toggleSign: null,
+  userName: "",
+  setUserName: () => {},
+  token: "",
+  login: (token) => {},
+  logout: () => {},
+  isLoggedIn: false,
+  googleSignIn: () => {},
+  googleSingOut: () => {},
+  user: "",
 });
 
-
 //?Defining our custom Hook
-export  const  useAuthContext=()=>{
-    return useContext(AuthContext)
+export const useAuthContext = () => {
+  return useContext(AuthContext);
 };
 
-const API_KEY=process.env.REACT_APP_APP_KEY;
-const url=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const API_KEY = process.env.REACT_APP_APP_KEY;
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 
+const AuthContextProvider = (props) => {
+  const [movieData, setMovieData] = useState("");
+  const [query, setQuery] = useState("");
+  const [toggleSign, setToggleSign] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState({}); //?for Google Auth
 
-const AuthContextProvider=(props)=>{
-    const initialToken=localStorage.getItem("token");
-    const [movieData,setMovieData]=useState("");
-    const [query, setQuery]=useState("");
-    const [toggleSign,setToggleSign]=useState(false);
-    const [userName,setUserName]=useState("");
-    const [token,setToken]=useState(null);
-    const [user,setUser]=useState({});//?for Google Auth
-
- 
-    const userIsLoggedIn=!!token || !!user?.accessToken;
-const fetchData=async()=>{
- try {
-  
-    const response= await fetch(url)
-    if(!response.ok){
-        throw new Error("Something went wrong")
-    }else{
-        const data= await response.json();
+  const userIsLoggedIn = !!token || !!user?.accessToken;
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      } else {
+        const data = await response.json();
         setMovieData(data);
-       
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
- } catch (error) {
-    console.log(error)
- }
-}
-
-const googleSignIn=()=>{
-const provider= new GoogleAuthProvider();
-signInWithPopup(auth,provider);
-
-}
-const googleSingOut=()=>{
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  const googleSingOut = () => {
     signOut(auth);
-}
-useEffect(()=>{
+  };
+  useEffect(() => {
     //?this will mount whenever we sign in with google
-    const unSubcribe=onAuthStateChanged(auth, (currentUser)=>{
-        setUser(currentUser);
-        // console.log("user", currentUser)
+    const unSubcribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      // console.log("user", currentUser)
     });
-    return ()=>{
-        unSubcribe()
-    }
-},[])
+    return () => {
+      unSubcribe();
+    };
+  }, []);
 
+  const loginHandler = (tokenId) => {
+    setToken(tokenId);
+    localStorage.setItem("token", token);
+  };
 
-const loginHandler=(tokenId)=>{
-setToken(tokenId);
-localStorage.setItem("token",token)
-}
-
-const logoutHandler=()=>{
+  const logoutHandler = () => {
     setToken(null);
     googleSingOut();
     localStorage.removeItem("name");
+  };
 
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-useEffect(()=>{fetchData()},[]);
-
-const searchMovie=useCallback(async()=>{
+  const searchMovie = useCallback(async () => {
     try {
-       const response= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`);
-       if(!response.ok){
-        throw new Error("Something went wrong")
-    }else{
-        const data= await response.json();
-        setMovieData(data)
-    }
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      } else {
+        const data = await response.json();
+        setMovieData(data);
+      }
     } catch (error) {
-        // console.log(error)
-        
+      // console.log(error)
     }
-},[query])
+  }, [query]);
 
-useEffect(()=>{searchMovie()},[searchMovie])
-const contextValue={
-movieData:movieData,
-setQuery:setQuery,
-toggleSign:toggleSign,
-setToggleSign:setToggleSign,
-userName:userName,
-setUserName:setUserName,
-token:token,
-login:loginHandler,
-logout:logoutHandler,
-isLoggedIn:userIsLoggedIn,
-googleSignIn:googleSignIn,
-googleSingOut:googleSingOut,
-user:user,
-fetchData:fetchData
+  useEffect(() => {
+    searchMovie();
+  }, [searchMovie]);
+  const contextValue = {
+    movieData: movieData,
+    setQuery: setQuery,
+    toggleSign: toggleSign,
+    setToggleSign: setToggleSign,
+    userName: userName,
+    setUserName: setUserName,
+    token: token,
+    login: loginHandler,
+    logout: logoutHandler,
+    isLoggedIn: userIsLoggedIn,
+    googleSignIn: googleSignIn,
+    googleSingOut: googleSingOut,
+    user: user,
+    fetchData: fetchData,
+  };
 
-}
-
-    return (<AuthContext.Provider value={contextValue}>
-        {props.children}
-    </AuthContext.Provider>)
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
